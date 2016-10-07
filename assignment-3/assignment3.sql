00f99f9da1a975292e61396365eb8541810eb8a6
mebra005 <mebra005@fiu.edu>
2016-10-07 15:13:41 -0400

    Update assignment3.sql

--- assignment-3/assignment3.sql
+++ assignment-3/assignment3.sql
@@ -13,3 +13,23 @@ UNION
 FROM	book
 WHERE	publishercode='JP')
 ORDER BY title;
+
+(SELECT	title
+FROM	book AS B
+WHERE	type ='CMP')
+UNION		
+(SELECT title
+FROM	book
+WHERE	type ='HIS')
+UNION
+(SELECT title
+FROM	book
+WHERE	type ='SCI')
+ORDER BY title;	
+
+
+SELECT	title
+FROM	author AS A, book AS B, wrote AS W
+WHERE	A.authornum = W.authornum AND B.bookcode = W.bookcode
+	AND A.authorlast ='Steinbeck' AND A.authorfirst ='John'
+ORDER BY title;
