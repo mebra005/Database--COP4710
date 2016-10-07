62a21f384817fdd1fdbcf4f3c90cac8ea15c15e5
mebra005 <mebra005@fiu.edu>
2016-10-07 15:00:42 -0400

    Update assignment3.sql

--- assignment-3/assignment3.sql
+++ assignment-3/assignment3.sql
@@ -4,3 +4,12 @@ WHERE	publishercode IN (SELECT	publishercode
 			  FROM		publisher
 			  WHERE		publishername = 'Penguin USA')
 			  ORDER BY title;
+			  
+(SELECT	title
+FROM	book AS B
+WHERE	type ='PSY')
+UNION		
+(SELECT title
+FROM	book
+WHERE	publishercode='JP')
+ORDER BY title;
